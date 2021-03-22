249012f026fb6db1c6a5f10ca83a1ee7bb7f8cab
John <souzajohnlee@gmail.com>
2021-03-22 13:54:05 -0700

    inject description data into p tag

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const card = document.getElementsByClassName('card')[0]
 const cardTitle = card.getElementsByTagName('h3')[0]
+const description = card.getElementsByTagName('p')[0]
 
 cardTitle.innerText = "Pad Thai"
+description.innerText = "John's favorite food"
